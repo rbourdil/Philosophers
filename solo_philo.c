139885b6bcca466fbb5fd0f7c67f59a07e06c4a7
Raphael BOURDILLAT <rbourdil@e1r4p19.clusters.42paris.fr>
2022-09-13 14:52:24 +0200

    added expeption for single philo time_to_die=0

--- solo_philo.c
+++ solo_philo.c
@@ -6,7 +6,7 @@
 /*   By: rbourdil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 18:08:12 by rbourdil          #+#    #+#             */
-/*   Updated: 2022/09/12 18:08:14 by rbourdil         ###   ########.fr       */
+/*   Updated: 2022/09/13 14:51:25 by rbourdil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*solo_philo(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *)arg;
+	if (isdead(philo->params, philo->id))
+		return (NULL);
 	print_action(philo->id, philo->params, FORK);
 	free(arg);
 	return (NULL);
